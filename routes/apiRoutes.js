6b416d3ee90143b62db489ca02a5316f355fa3a6
Ray Ray <raymond2129@gmail.com>
2019-03-10 18:27:05 -0600

    Updated node mailer and login type

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,125 @@
 var db = require("../models");
+var moment = require('moment');
+var request = require('request');
+var nodemailer = require('nodemailer');
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+module.exports = function (app) {
+
+  app.get("/", function (req, res) {
+    res.render("index");
+  });
+  app.get("/survey", function (req, res) {
+    res.render("survey");
+  });
+  app.get("/profile", function (req, res) {
+    res.render("profile");
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+
+  // POST route for new user
+  app.post("/api/add", function (req, res) {
+
+    console.log(req.body);
+
+    db.Users.create({
+      email: req.body.email,
+      topCoffee: req.body.topCoffee,
+      secondCoffee: req.body.secondCoffee,
+      uid: req.body.uid
+    }).then(function (results) {
+      res.json(results);
     });
+
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  //GET route for MYQL id (when new user is created)
+  app.get("/new/:id", function (req, res) {
+    db.Users.findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function (result) {
+
+        
+
+
+        request(queryURL, function (error, response, body) {
+          console.log('error:', error); // Print the error if one occurred
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          res.render("profile", JSON.parse(body));
+        });
+      });
+  });
+
+  //GET route for FB id (when existing user logs in)
+  app.get("/current/:uid", function (req, res) {
+    db.Users.findOne({
+        where: {
+          uid: req.params.uid
+        }
+      })
+      .then(function (result) {
+
+
+        request(queryURL, function (error, response, body) {
+          console.log('error:', error); // Print the error if one occurred
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          res.render("profile", JSON.parse(body));
+        });
+      });
+  });
+
+  //POST route for nodemailer
+  app.post("/email", function (req, res) {
+    var recipient = req.body.email;
+
+    db.Users.findOne({
+        where: {
+          email: recipient
+        }
+      })
+      .then(function (result) {
+
+        request(queryURL, function (error, response, body) {
+          console.log('error:', error); // Print the error if one occurred
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          var moviesBody = JSON.parse(body);
+          //var imgToEmail = " <img src='coffee type"' />";
+
+          console.log("Nodemailer sending to: " + recipient);
+
+          var transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: "coffeequiz303@gmail.com",
+              pass: "Root123!@#"
+            }
+          });
+
+          var mailOptions = {
+            from: "coffeequiz303@gmail.com",
+            replyTo: "coffeequiz303@gmail.com",
+            to: recipient,
+            subject: "Here are your results from the Coffee Quiz!",
+            text: "Need to list the results here"
+          };
+
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent');
+            }
+          });
+
+          console.log("COFFEE TYPE EMAILED: " + "need to send that type");
+
+          res.send(true);
+
+        });
+
+      });
+
   });
 };
