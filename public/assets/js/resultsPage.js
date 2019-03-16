93bb77bc094985dfa1bc7caae0438b63acf8855a
Ashinator <Ahart425@gmail.com>
2019-03-16 13:44:11 -0600

    add re-do results button

--- public/assets/js/resultsPage.js
+++ public/assets/js/resultsPage.js
@@ -2,9 +2,21 @@ $(document).ready(function() {
   $("#submit").on("click", function() {
 
 
-  });
-
-$("#username").append("#username"); 
 
 
-});
+    // APPEND USER NAME TO TOP RESULT BOX
+    $("#username").append("#firstname");
+
+    // APPEND USER COFFE TYPE RESULT TO TOP RESULT BOX
+    $("#coffeeResult").append("");
+
+    // APPEND DESCRIPTION OF THE USERS COFFEE TYPE RESULT
+    $("#resultDescription").append("");
+
+    // APPEND USER MATCHES TO TABS
+
+
+
+
+  }); // END ON ONCLICK SUBMIT FUNCTION
+}); // END DOCUMENT READY FUNCTION
