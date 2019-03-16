e904d388b65ce0a0f5d92e122c4751d1916b436f
Ashinator <Ahart425@gmail.com>
2019-03-16 12:58:03 -0600

    adding javascript

--- public/assets/js/resultsPage.js
+++ public/assets/js/resultsPage.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-
-$("#nextArrowOne").on("click", function() {
-
+  $("#submit").on("click", function() {
 
 
+  });
+
+$("#username").append("#username"); 
 
 
-
-}
+});
