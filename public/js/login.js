ddb2c8ad3539f95664cfe5385c28342ab03931d0
Ray Ray <raymond2129@gmail.com>
2019-03-17 17:50:05 -0600

    fixed the handlebars css loading

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,23 @@
-var handleCreateAccount = () => {
-    event.preventDefault();
-  
-    var username = $('#createUsername').val().trim();
-    var name = $('#name').val().trim();
-    var password = $('#createPassword').val();
-    var age = $('#age').val().trim();
-    var location = $('#location').val().trim();
-  
-    addUser({
-      username: username,
-      name: name,
-      password: password,
-      age: age,
-      location: location
-    });
-  
-  
-  }
-  
-  let addUser = (userData) => {
-    $.post('/create-account', userData);
-  }
-  
-  $('#createAccount').on('click', handleCreateAccount);
+var handleCreateAccount = function() {
+  event.preventDefault();
+
+  // var userName = $("#user-name").val().trim();
+  // var email = $("#email").val().trim();
+  // var password = $('#createPassword').val();
+  // var age = $('#age').val().trim();
+  // var location = $('#location').val().trim();
+
+  addUser({
+    userName: userName,
+    email: email
+    // password: password,
+    // age: age,
+    // location: location
+  });
+};
+
+var addUser = function(userData){
+  $.post("/api/add", userData);
+};
+
+$("#createAccount").on("click", handleCreateAccount);
